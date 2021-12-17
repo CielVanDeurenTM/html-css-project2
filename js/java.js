be4a95a9da166f1077b13634774e288ae55884c0
CielVanDeuren <cielvandeuren@gmail.com>
2021-12-17 14:31:34 +0100

    CV.html almost done, starting on Ervaringen

--- js/java.js
+++ js/java.js
@@ -1,3 +1,4 @@
+//footer
 window.onscroll = function() {myFunction2()};
 
 var navbar = document.getElementById("myTopnav");
@@ -17,7 +18,6 @@ function myFunction2() {
 window.onresize = function() {myFunction3()};
 window.onload = function() {myFunction3()};
 
-
 var footer = document.getElementById("footer") 
 
 function myFunction3(){
@@ -29,11 +29,7 @@ function myFunction3(){
   }  
 }
 
-
-
-
-
-
+//responsive navbar
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -42,4 +38,34 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+//leeftijd
+window.onload = function() {myFunction4()}
+
+function myFunction4() {
+
+  var d1 = "09"
+  var m1 = "01"
+  var y1 = "2000"
+
+  var date = new Date();
+  var d2 = date.getDate();
+  var m2 = 1 + date.getMonth();
+  var y2 = date.getFullYear();
+  var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
   
+  if(d1 > d2){
+    d2 = d2 + month[m2 - 1];
+    m2 = m2 - 1;
+  }
+  if(m1 > m2){
+    m2 = m2 + 12;
+    y2 = y2 - 1;
+  }
+
+  var d = d2 - d1;
+  var m = m2 - m1;
+  var y = y2 - y1;
+
+  document.getElementById('leeftijd').innerText = y+" jaar oud";
+}
