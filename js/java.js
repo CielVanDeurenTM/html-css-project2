ccb21870f46688a181694eb55c2983805fd5d087
CielVanDeuren <cielvandeuren@gmail.com>
2021-12-19 16:08:19 +0100

    index.html done + footer and cv changes

--- js/java.js
+++ js/java.js
@@ -40,34 +40,3 @@ function myFunction() {
       x.className = "topnav";
     }
   }
-//leeftijd
-window.onload = function() {myFunction4()}
-
-function myFunction4() {
-
-  var d1 = "09"
-  var m1 = "01"
-  var y1 = "2000"
-
-  var date = new Date();
-  var d2 = date.getDate();
-  var m2 = 1 + date.getMonth();
-  var y2 = date.getFullYear();
-  var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-  
-  if(d1 > d2){
-    d2 = d2 + month[m2 - 1];
-    m2 = m2 - 1;
-  }
-  if(m1 > m2){
-    m2 = m2 + 12;
-    y2 = y2 - 1;
-  }
-
-  var d = d2 - d1;
-  var m = m2 - m1;
-  var y = y2 - y1;
-
-  document.getElementById('leeftijd').innerText = y+" jaar oud";
-}
